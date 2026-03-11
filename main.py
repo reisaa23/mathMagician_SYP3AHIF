de14212da8749921995af337ac1e4fac45b3f70b
Edi Sadikaj <sadikaj.edi@pm.me>
2026-03-11 13:56:38 +0100

    Add simple subtraction function

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ def main():
       a = float(sys.argv[2])
       b = float(sys.argv[3])
 
-      
+
 if __name__ == "__main__":
       main()
 
 def add(a, b):
       return a + b
-  
+
 def subtract(a, b):
       """Subtract b from a"""
       return a - b
+
+def division(a,b);
+    """Divide a by b"""
+    if b == 0:
+        raise ZeroDivisionError("Division by zero is not allowed")
+    return a / b
